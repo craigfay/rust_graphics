c1d7344912a770da0a6e4d8fe9455a95611187ac
craig fay <craigfay1@gmail.com>
2020-06-10 20:18:30 -0500

    Introduce render()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::WindowCanvas;
 use std::time::Duration;
+
+
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
+}
+
+
+
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -15,15 +26,10 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -33,9 +39,14 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+
+        // Time Management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
