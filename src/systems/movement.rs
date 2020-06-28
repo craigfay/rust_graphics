cf017979085567241d2788ca146a8b76e8be40a4
craig fay <craigfay1@gmail.com>
2020-06-28 18:50:23 -0500

    Use position_is_obstructed()

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -5,6 +5,7 @@ use amethyst::input::{InputHandler, StringBindings};
 
 use crate::simple_game::{
     TileOccupant,
+    Position,
     DISPLAY_WIDTH,
     DISPLAY_HEIGHT,
     TILE_ROWS,
@@ -14,6 +15,12 @@ use crate::simple_game::{
 #[derive(SystemDesc)]
 pub struct MovementSystem;
 
+
+fn position_is_obstructed(p: &Position) -> bool {
+    if p.y < 0 || p.y >= TILE_ROWS as i8 { return true }
+    if p.x < 0 || p.x >= TILE_COLUMNS as i8 { return true }
+    false
+}
 
 impl<'s> System<'s> for MovementSystem {
     // Defining the data that the system
@@ -33,20 +40,25 @@ impl<'s> System<'s> for MovementSystem {
             let leftright = input.axis_value("leftright");
 
             if let Some(mv_amount) = updown {
-                let new_y_position = occupant.position.y + mv_amount as i8;
-                if new_y_position >= 0 && new_y_position < TILE_ROWS as i8 {
+                let new_position = Position {
+                    x: occupant.position.x,
+                    y: occupant.position.y + mv_amount as i8,
+                };
+                if !position_is_obstructed(&new_position) {
                     let scaled_amount = 1.2 * mv_amount as f32;
-                    occupant.position.y = new_y_position;
+                    occupant.position.y = new_position.y;
                     transform.prepend_translation_y(scaled_amount);
                 }
-
             }
 
             if let Some(mv_amount) = leftright {
-                let new_x_position = occupant.position.x + mv_amount as i8;
-                if new_x_position >= 0 && new_x_position < TILE_COLUMNS as i8 {
+                let new_position = Position {
+                    x: occupant.position.x + mv_amount as i8,
+                    y: occupant.position.y,
+                };
+                if !position_is_obstructed(&new_position) {
                     let scaled_amount = 1.2 * mv_amount as f32;
-                    occupant.position.x = new_x_position;
+                    occupant.position.x = new_position.x;
                     transform.prepend_translation_x(scaled_amount);
                 }
             }
